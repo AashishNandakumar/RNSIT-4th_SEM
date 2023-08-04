4bc07c5d1585ac40c72b9a9310ded471e6ae3377
Aashish <aashishnkumar@gmail.com>
2023-08-04 18:05:54 +0530

    finished greedy knapsack algorithm

--- DAA/src/GreedyKnapsackAlgorithm/Knapsack.java
+++ DAA/src/GreedyKnapsackAlgorithm/Knapsack.java
@@ -10,6 +10,7 @@ public class Knapsack {
         ArrayList<Integer> profit = new ArrayList<>();
         ArrayList<Integer> weight = new ArrayList<>();
         ArrayList<Double> partsOfObject = new ArrayList<>();
+        ArrayList<Integer> tempIndex = new ArrayList<>();
 
         in = new Scanner(System.in);
         System.out.println("Enter knapsack capacity: ");
@@ -25,19 +26,18 @@ public class Knapsack {
             int p = in.nextInt();
             weight.add(w);
             profit.add(p);
-
+            tempIndex.add(i+1);
         }
         in.close();
 
-//        ArrayList<Integer> tempIndex = new ArrayList<>();
         for(int i=0;i<noOfObjects-1;i++){
             for(int j=0;j<noOfObjects-i-1;j++){
 
                 if(profit.get(j)/weight.get(j) < profit.get(j+1)/weight.get(j+1)){
-//                    tempIndex.add(j);
 
                     Collections.swap(profit, j, j+1);
                     Collections.swap(weight, j, j+1);
+                    Collections.swap(tempIndex, j, j+1);
                 }
 
             }
@@ -61,15 +61,14 @@ public class Knapsack {
         }
 
         System.out.println("\nResult: ");
-        System.out.println("Profit: "+profit);
-        System.out.println("Weight: "+weight);
+
         System.out.println("Summation XW: "+sumXW);
         System.out.println("Summation XP: "+sumXP);
         System.out.println("Solution vector: ");
 
-//        for(int i=0;i<noOfObjects;i++){
-//            System.out.println("X"+(tempIndex.get(i))+": "+partsOfObject.get(i));
-//        }
+        for(int i=0;i<noOfObjects;i++){
+            System.out.println("X"+(tempIndex.get(i))+": "+partsOfObject.get(i));
+        }
     }
 
 
