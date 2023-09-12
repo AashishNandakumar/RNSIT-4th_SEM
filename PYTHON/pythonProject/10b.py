5fb20d8dce34d7c27e822568db255b36dc7c34d6
Aashish <aashishnkumar@gmail.com>
2023-09-12 18:35:22 +0530

    Added 10b

---
+++ PYTHON/pythonProject/10b.py
@@ -0,0 +1,24 @@
+import json
+
+
+def fetch_and_format_weather(json_file):
+    try:
+        with open(json_file, 'r') as file:
+            data = json.load(file)
+
+        temperature = data['main']['temp']
+        humidity = data['main']['humidity']
+        weather_description = data['weather'][0]['description']
+
+        print(f"Current temperature: {temperature: .2f} Celsius")
+        print(f"Humidity: {humidity}%")
+        print(f"weather description: {weather_description}")
+
+    except FileNotFoundError:
+        print(f"file {json_file} does not exist")
+    except KeyError:
+        print("JSON file does not contain the expected weather")
+
+
+json_file_path = "weather_data.json"
+fetch_and_format_weather(json_file_path)
---
+++ PYTHON/pythonProject/weather_data.json
@@ -0,0 +1,43 @@
+{
+  "coord": {
+    "lon": -73.99,
+    "lat": 40.73
+  },
+  "weather": [
+    {
+      "id": 800,
+      "main": "Clear",
+      "description": "clear sky",
+      "icon": "01d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 15.45,
+    "feels_like": 12.74,
+    "temp_min": 14.44,
+    "temp_max": 16.11,
+    "pressure": 1017,
+    "humidity": 64
+  },
+  "visibility": 10000,
+  "wind": {
+    "speed": 4.63,
+    "deg": 180
+  },
+  "clouds": {
+    "all": 1
+  },
+  "dt": 1617979985,
+  "sys": {
+    "type": 1,
+    "id": 5141,
+    "country": "US",
+    "sunrise": 1617951158,
+    "sunset": 1618000213
+  },
+  "timezone": -14400,
+  "id": 5128581,
+  "name": "New York",
+  "cod": 200
+}
